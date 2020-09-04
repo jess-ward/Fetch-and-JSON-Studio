e779c559c26e485dcaf549cf9c1db35529de2196
Jessica <jrnewport07@gmail.com>
2020-09-04 12:43:00 -0500

    alternative ways to add bonus features

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ window.addEventListener("load", function(){
     // console.log(response);
     response.json().then(function(json){
         // console.log(json);
+        container.innerHTML = 
+            `<div>
+                <h2>Crew Number: ${json.length}</h2>
+            </div>`
         for (let i=0;i<json.length;i++){
         const container= document.getElementById("container");
         json.sort(function(a,b){
@@ -14,8 +18,8 @@ window.addEventListener("load", function(){
         <h3>${json[i].firstName} ${json[i].lastName}</h3>
         <ul>
          <li>Hours in space: ${json[i].hoursInSpace}</li>
-         <li>Active: ${json[i].active}</li>
-         <li>Skills: ${json[i].skills}</li>
+         ${(json[i].active === true) ? `<li style = "color: green">Active: ${json[i].active}</li>` : `<li id = "active">Active: ${json[i].active}</li>`}
+         <li>Skills: ${json[i].skills.join(', ')}</li>
       </ul>
    </div>
    <img class="avatar" src=${json[i].picture}>
